266d5b67f32973de5c1bdf7d46af8cb0ea84a35a
Tomoki Yoshizawa <tomoki-goalie@hotmail.com>
2024-03-12 08:25:37 -0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+import "./App.css";
+
+function App() {
+  const pokeApi = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {}, []);
+
+  return <div className="app">app</div>;
+}
+
+export default App;
